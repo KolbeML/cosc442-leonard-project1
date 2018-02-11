47a75d3f6ac404179255fbadb02e5bae1e46dc9f
KolbeML <kleona3@students.towson.edu>
2018-02-11 17:04:22 -0500

    Change playAction - now takes string param 'msg', returns boolean.

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/UtilityCellTest.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/UtilityCellTest.java
@@ -46,7 +46,7 @@ public class UtilityCellTest extends TestCase {
 		gameMaster.getPlayer(0).purchase();
 		gameMaster.switchTurn();
 		gameMaster.movePlayer(1, cellIndex);
-		cell.playAction();
+		cell.playAction(null);
 		int diceRoll = gameMaster.getUtilDiceRoll();
 		assertEquals(
 				1500 - cell.getRent(diceRoll),
